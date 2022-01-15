2fc23b9faacf39dd5f7d1cef8d6816f761ddbe4e
kazemipour76 <ali.kazemipour@yahoo.com>
2022-01-15 17:39:54 +0330

    example3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-      //  System.out.println("enter your name");
+        System.out.println("enter your name");
         Scanner scanner = new Scanner(System.in);
-      //  String name = scanner.nextLine().toUpperCase();
+        String name = scanner.nextLine().toUpperCase();
 
         System.out.println("lastName");
         String lastName = scanner.nextLine().toUpperCase();
 
-       // System.out.println("nationalCode");
-       // int nationalCode = scanner.nextInt();
+        System.out.println("nationalCode");
+        String nationalCode = scanner.nextLine();
 
        System.out.println("age");
        String age = scanner.nextLine();
 
-        //System.out.println(" your name is " +name  + "and your lastName is "+ lastName +  "and your age is "+ age +  "and your nationalCod is "+ nationalCode) ;
-
         int newAge= Integer.parseInt(age);
-        System.out.println(newAge+2);
+        //System.out.println(newAge+2);
 
-        String
+       // String agenew = String.valueOf(age);
+       // System.out.println(agenew+2);
+        int newCode =Integer.parseInt(nationalCode);
+
+        System.out.println(" your name is " +name  + "and your lastName is "+ lastName +  "and your age is " + age +  " and newAge+2 "+ newAge+2 +  " and newCode "+ newCode) ;
     }
 }
 
