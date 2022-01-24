e5350424be92120b60af47a62ec3777e4c3ae6e8
kazemipour76 <ali.kazemipour@yahoo.com>
2022-01-24 17:32:44 +0330

    PRACTICE 11

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -6,33 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Mobile mobile1 = new Mobile();
-        mobile1.camera=13;
-        mobile1.company="samsung";
-        mobile1.model="j2016";
-        mobile1.name="galaxy j7";
-        mobile1.price=200000;
+      //  Mobile mobile1 = new Mobile();
+       // mobile1.camera=13;
+       // mobile1.company="samsung";
+       // mobile1.model="j2016";
+       // mobile1.name="galaxy j7";
+       // mobile1.price=200000;
 
-        System.out.println("Camera is "+mobile1.camera);
-        System.out.println("company is "+mobile1.company);
-        System.out.println("model is "+mobile1.model);
-        System.out.println("name is "+mobile1.name);
-        System.out.println("price is "+mobile1.price);
+       // System.out.println("Camera is "+mobile1.camera);
+       // System.out.println("company is "+mobile1.company);
+       // System.out.println("model is "+mobile1.model);
+       // System.out.println("name is "+mobile1.name);
+       // System.out.println("price is "+mobile1.price);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(  "enter code");
+        int code=scanner.nextInt();
+        System.out.println(  "enter age");
+        int age=scanner.nextInt();
 
+        System.out.println(  "enter name");
+        String name=scanner.next();
+        System.out.println(  "enter family");
+        String family=scanner.next();
+        Person person = new Person(code,age,name,family);
 
-        Mobile mobile2 = new Mobile();
-        mobile2.camera=13;
-        mobile2.company="samsung";
-        mobile2.model="j2016";
-        mobile2.name="galaxy j7";
-        mobile2.price=200000;
+        System.out.println(  "age is "+person.age);
+        System.out.println( "code is "+ person.code);
+        System.out.println(  "name is "+person.name);
+        System.out.println( "family is "+ person.family);
 
-        System.out.println("Camera is "+mobile2.camera);
-        System.out.println("company is "+mobile2.company);
-        System.out.println("model is "+mobile2.model);
-        System.out.println("name is "+mobile2.name);
-        System.out.println("price is "+mobile2.price);
     }
 }
 
---
+++ src/oop/Person.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Person {
+
+
+    public Person(int code ,int age,String name,String family){
+        this.age=age;
+        this.code=code;
+        this.name=name;
+        this.family=family;
+    }
+
+    public int code;
+    public int age;
+    public String name;
+    public String family;
+
+}
